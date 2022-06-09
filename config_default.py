d28d70fb81f0cf640c674112c2cca470bd714636
Boyao SONG <85090456+akaigua@users.noreply.github.com>
2022-06-09 14:57:36 +0800

    minor fix

---
+++ config_default.py
@@ -0,0 +1,8 @@
+class DetaConfig:
+
+    """
+    Note that specifying `PROJECT_KEY` is optional and should be added only while debugging.
+    Deta will help you add `PROJECT_KEY` on server automatically.
+    """
+    PROJECT_KEY = "ENTER_PROJECT_KEY_HERE"
+    BASE_NAME = "shortlink_db"
